db22b9be8236e60305b67d0cbd43611aad56d700
LeMonkii <ianisbrebion@gmail.com>
2022-01-09 11:34:27 +0000

    Deploy https://github.com/LeMonkii/LeMonkii.github.io/commit/54d78cff580468aa98e7a309e2fad6b28680bedf

---
+++ assets/client.c31e23d3.js
@@ -0,0 +1,1 @@
+import{v as r,S as o}from"./vendor.ee77b57e.js";const a=({value:t})=>t?r("astro-fragment",{dangerouslySetInnerHTML:{__html:t}}):null;a.shouldComponentUpdate=()=>!1;var m=a,u=t=>(n,l,e)=>o(r(n,l,e!=null?r(m,{value:e}):e),t);export{u as default};
