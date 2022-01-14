da94463cea019f46447da7fc90e65be411d83b00
lemonkii <ianisbrebion@gmail.com>
2022-01-14 22:26:37 +0100

    update contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,18 +1,30 @@
 import { h } from "preact";
 import { useState } from "preact/hooks";
+import Cookies from "js-cookie";
 
 function Contact() {
-  const [displayContact, setDisplayContact] = useState(false);
+  const [displayContact, setDisplayContact] = useState(
+    !!Cookies.get("contact") ?? false
+  );
+
+  const displayInformations = () => {
+    Cookies.set("contact", "1");
+    setDisplayContact(true);
+  };
 
   return (
-    <div className="contact" onClick={() => setDisplayContact(true)}>
+    <div className="contact" onClick={() => displayInformations()}>
       {!displayContact && (
         <span>Cliquez ici pour afficher mes coordonnées</span>
       )}
       {displayContact && (
         <div className="contact__content">
-          <a href="mailto:ianisbrebion@gmail.com">ianisbrebion@gmail.com</a>
-          <a href="tel:+33679399825">+33 6 79 39 98 25</a>
+          <div>
+            <a href="mailto:ianisbrebion@gmail.com">ianisbrebion@gmail.com</a>
+          </div>
+          <div>
+            <a href="tel:+33679399825">+33 6 79 39 98 25</a>
+          </div>
         </div>
       )}
     </div>
